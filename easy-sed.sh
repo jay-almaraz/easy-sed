f65ecff01ab10345e5b16c268351f82dd87df5ee
jay-almaraz <jay.amgrant@gmail.com>
2019-02-27 20:16:07 +1000

    Add OSX compatability without wrecking everything else

--- easy-sed.sh
+++ easy-sed.sh
@@ -13,12 +13,6 @@ FIND=$1
 REPLACE=$2
 FILE=$3
 
-# echo "find:     ${FIND}"
-# echo "replace:  ${REPLACE}"
-# echo "file:     ${FILE}"
-
-# FIND=$(sed 's|*|\\*|g' <<< ${FIND})
-# FIND=$(sed 's|_|\\_|g' <<< ${FIND})
 FIND=$(sed 's|*|\\*|g' <<< ${FIND})
 FIND=$(sed 's|_|\\_|g' <<< ${FIND})
 FIND=$(sed 's|&|\\&|g' <<< ${FIND})
@@ -29,12 +23,14 @@ REPLACE=$(sed 's|_|\\_|g' <<< ${REPLACE})
 REPLACE=$(sed 's|&|\\&|g' <<< ${REPLACE})
 REPLACE=$(sed 's|'\''|'\'"\\\'"\''|g' <<< ${REPLACE})
 
-# echo "${FIND}"
-# echo "${REPLACE}"
-# exit 0
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    EASY_SED_COMMAND="sed -i ''"
+else
+    EASY_SED_COMMAND='sed -i'
+fi
 
 if [ "${#}" == 3 ]; then
-    COMMAND="sed -i 's_${FIND}_${REPLACE}_g' ${FILE}"
+    COMMAND="${EASY_SED_COMMAND} 's_${FIND}_${REPLACE}_g' ${FILE}"
 else
     COMMAND="sed 's_${FIND}_${REPLACE}_g'"
 fi
